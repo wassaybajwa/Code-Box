6bb556cb7311d6eb8d2ef7ecb43cd12d2c52712e
Wassay Bajwa <71424282+wassaybajwa@users.noreply.github.com>
2021-07-31 20:38:54 -0400

    Login/Register rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Login from "./Login";
+import Form from "./Form"
 
-var isLoggedIn = true;
-const currentTime = new Date().getHours();
+// var isLoggedIn = true;
+// const currentTime = new Date().getHours();
 
-console.log(currentTime);
+var userIsRegistered = true;
+
+// console.log(currentTime);
 
 function App() {
   return (
-    <div className="container">{
-        // isLoggedIn ? <h1>Hello</h1> : <Login />
-        currentTime > 1 && <h1>Why are you still working??</h1>
-    }
+    <div className="container">
+        {/* // isLoggedIn ? <h1>Hello</h1> : <Login /> */}
+        {/* currentTime > 1 && <h1>Why are you still working??</h1> */}
+        <Form isRegistered={userIsRegistered}/>
+    
     </div>
   );
 }

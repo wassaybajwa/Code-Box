a20c50517cf811668bbb2019ee6c286ef4670297
Wassay Bajwa <71424282+wassaybajwa@users.noreply.github.com>
2021-08-01 04:53:30 -0400

    Destructuring Array and rendering in React

--- src/index.js
+++ src/index.js
@@ -1,42 +1,34 @@
-// CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
-import animals, { useAnimals } from "./data";
-// import Data from "./data";
+import React from "react";
+import ReactDOM from "react-dom";
+import Cars  from "./cars";
 
-const [cat, dog] = animals
+const [honda, tesla] = Cars;
 
-// const {name: catName, sound: catSound} = cat;
+const {speedStats: {topSpeed: hondaTopSpeed}} = honda;
 
-// const {name="fulffy", sound="Purr"} = cat;
+const {speedStats: {topSpeed: teslaTopSpeed}} = tesla;
 
-// const {name, sound, feedingRequirements: {food, water}} = cat;
+const {coloursByPopularity: [hondaTopColour]} = honda;
 
-// const {feedingRequirements: {food, water}} = cat;
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const [animal, makeSound] = useAnimals(cat)
-console.log(animal);
-makeSound();
+const {coloursByPopularity: [teslaTopColour]} = tesla;
 
 
-
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);

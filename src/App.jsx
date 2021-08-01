1bb981018b3d0840870f773588a91abc07cd323b
Wassay Bajwa <71424282+wassaybajwa@users.noreply.github.com>
2021-08-01 00:21:08 -0400

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,55 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
+
 
 function App() {
-    const [count, setCount] = useState(0);
+    setInterval(updateTime, 1000)
+    const now =  new Date().toLocaleTimeString();
+    
+    const [time, setTime] = useState(now)
 
-    const [red, green, blue] = [9, 132, 227]
-    
-    function increase() {
-      //count++
-      setCount(count + 1)
-    } 
-    function decrease() {
-        //count++
-        setCount(count - 1)
-      } 
-    
+    function updateTime() {
+
+        const newTime = new Date().toLocaleTimeString();
+        setTime(newTime)
+    }
     return (
-      <div className="container">
-        {/* <h1>{state[0]}</h1>  */}
-        <h1>{count}</h1> 
-
-        <button onClick={increase}>+</button>
-        <button onClick={decrease}>-</button>
-      </div>
-      
-    );
-    
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
+    </div>
+  );
 }
 
 export default App;
+
+
+// import React, { useState } from "react";
+
+// function App() {
+//     const [count, setCount] = useState(0);
+
+//     const [red, green, blue] = [9, 132, 227]
+    
+//     function increase() {
+//       //count++
+//       setCount(count + 1)
+//     } 
+//     function decrease() {
+//         //count++
+//         setCount(count - 1)
+//       } 
+    
+//     return (
+//       <div className="container">
+//         {/* <h1>{state[0]}</h1>  */}
+//         <h1>{count}</h1> 
+
+//         <button onClick={increase}>+</button>
+//         <button onClick={decrease}>-</button>
+//       </div>
+      
+//     );
+    
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root")
-);
+//Challenge:
+//1. Given that you can get the current time using:
 
+//Show the latest time in the <h1> when the Get Time button
+//is pressed.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//2. Given that you can get code to be called every second
+//using the setInterval method.
+//Can you get the time in your <h1> to update every second?
+
+//e.g. uncomment the code below to see Hey printed every second.
+// function sayHi() {
+//   console.log("Hey");
+// }
+// setInterval(sayHi, 1000);
